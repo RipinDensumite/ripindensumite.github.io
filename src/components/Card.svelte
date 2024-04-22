2cb9fc485a8951fcd4260f67df419261c59179d6
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-04-22 13:56:49 +0800

    Update card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="card w-96 border-2 border-gray-800 my-3 hover:-translate-y-2 hover:shadow-lg transition-all"
+  class="card w-96  border-2 border-gray-800 my-3 hover:-translate-y-2 hover:shadow-lg transition-all"
 >
   <div class="card-body">
     <h2 class="card-title">{title}</h2>
