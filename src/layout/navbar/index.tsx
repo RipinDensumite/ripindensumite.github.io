3ea87dbb2f050d96d3c1de6c9e7d763a8f26e670
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-09-19 21:13:06 +0800

    feat: Turn navItems into object array

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -1,18 +1,27 @@
 function NavBarLayout() {
+
+    const navItems = [
+        { href: "#about", label: "About" },
+        { href: "#skills", label: "Skills" },
+        { href: "#achievements", label: "Achievements" },
+        { href: "#qualifications", label: "Qualifications" },
+        { href: "#projects", label: "Projects" },
+        { href: "#contact", label: "Contact" },
+    ]
+
     return (
         <header className="flex justify-between items-center mb-8 max-w-2xl mx-auto">
             <div className="flex space-x-4">
-                <button className="text-2xl">☼</button>
+                {/* <button className="text-2xl">☼</button>
                 <button className="text-2xl">☾</button>
-                <button className="text-2xl">⧉</button>
+                <button className="text-2xl">⧉</button> */}
             </div>
-            <nav>
-                <a href="#about" className="mr-4 underline">about</a>
-                <a href="#skills" className="mr-4 underline">skills</a>
-                <a href="#achievements" className="mr-4 underline">achievements</a>
-                <a href="#qualifications" className="mr-4 underline">qualifications</a>
-                <a href="#projects" className="mr-4 underline">projects</a>
-                <a href="#contact" className="underline">contact</a>
+            <nav className="overflow-auto">
+                {
+                    navItems.map((item, index) => (
+                        <a key={index} href={item.href} className="mr-4 underline">{item.label}</a>
+                    ))
+                }
             </nav>
         </header>
     )
