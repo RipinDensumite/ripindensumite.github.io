33641062e1582e8a33bfba220ffdeee0295695c9
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-09-19 21:13:30 +0800

    feat: Add calendarDays icon

--- src/modules/qualifications/index.tsx
+++ src/modules/qualifications/index.tsx
@@ -1,3 +1,5 @@
+import { CalendarDays } from "lucide-react"
+
 export default function QualificationsModule() {
 
     const qualifications = [
@@ -18,10 +20,11 @@ export default function QualificationsModule() {
             <h2 className="text-2xl font-bold mb-4">QUALIFICATIONS</h2>
             <ul className="list-disc list-inside flex flex-col gap-5">
                 {qualifications.map(qualification => (
-                    <li className="flex flex-col">
+                    <li className="flex flex-col gap-1">
                         <span>{qualification.title},</span>
                         <span>{qualification.institution},</span>
-                        <span className="whitespace-nowrap bg-black px-2.5 py-0.5 text-sm text-white w-fit">{qualification.duration}</span>
+                        {/* <span className="whitespace-nowrap bg-black px-2.5 py-0.5 text-sm text-white w-fit">{qualification.duration}</span> */}
+                        <span className="flex items-center gap-2"><CalendarDays className="w-4 h-4" />{qualification.duration}</span>
                     </li>
                 ))}
             </ul>
