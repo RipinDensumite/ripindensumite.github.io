e2c128a63e8b577c1b4949a1c6ffc40e4dc15ebf
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-09-19 11:31:32 +0800

    feat: Update qualification

--- src/modules/qualifications/index.tsx
+++ src/modules/qualifications/index.tsx
@@ -1,11 +1,29 @@
 export default function QualificationsModule() {
+
+    const qualifications = [
+        {
+            title: "Diploma in Software Engineering (Software Testing)",
+            institution: "German-Malaysian Institute",
+            duration: "July 2022 - June 2025"
+        },
+        {
+            title: "Sijil Pelajaran Malaysia",
+            institution: "Sekolah Menengah Teknik Kuala Lumpur",
+            duration: "January 2020 - March 2022"
+        }
+    ]
+
     return (
         <section id="qualifications">
             <h2 className="text-2xl font-bold mb-4">QUALIFICATIONS</h2>
-            <ul className="list-disc list-inside">
-                <li>B.S. in Computer Science, [University Name] (Expected [Graduation Year])</li>
-                <li>[Any relevant certifications]</li>
-                <li>[Any relevant coursework]</li>
+            <ul className="list-disc list-inside flex flex-col gap-5">
+                {qualifications.map(qualification => (
+                    <li className="flex flex-col">
+                        <span>{qualification.title},</span>
+                        <span>{qualification.institution},</span>
+                        <span className="whitespace-nowrap bg-black px-2.5 py-0.5 text-sm text-white w-fit">{qualification.duration}</span>
+                    </li>
+                ))}
             </ul>
         </section>
     )
