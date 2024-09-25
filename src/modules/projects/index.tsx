4f6cce3d48c1a0982d1af343269fdfeb02edd8b0
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-09-25 13:08:37 +0800

    feat: Remove border duplication

--- src/modules/projects/index.tsx
+++ src/modules/projects/index.tsx
@@ -44,7 +44,7 @@ export default function ProjectsModule() {
             <div className="space-y-4">
                 {
                     projects.map((project, index) => (
-                        <main key={index} className="border border-gray-800 p-5 flex flex-row justify-between">
+                        <main key={index} className="border-2 rounded-md border-gray-800 p-5 flex flex-row justify-between">
                             <section>
                                 <h3 className="font-bold">{project.name}</h3>
                                 <p>{project.description}</p>
@@ -64,8 +64,8 @@ export default function ProjectsModule() {
                                 </div>
                                 <a href={project.url} className="underline">View Project</a>
                             </section>
-                            {project.status === "live" && <span className="bg-emerald-100 px-2.5 py-0.5 border border-2 border-emerald-700 text-emerald-700 h-fit">live</span>}
-                            {project.status === "offline" && <span className="bg-red-100 px-2.5 py-0.5 border border-2 border-red-700 text-red-700 h-fit">offline</span>}
+                            {project.status === "live" && <span className="bg-emerald-100 px-2.5 py-0.5 border-2 border-emerald-700 text-emerald-700 h-fit">live</span>}
+                            {project.status === "offline" && <span className="bg-red-100 px-2.5 py-0.5 border-2 border-red-700 text-red-700 h-fit">offline</span>}
                         </main>
                     ))
                 }
