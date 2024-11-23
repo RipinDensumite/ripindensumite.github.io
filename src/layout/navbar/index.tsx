f7d94e776e3880e7cab6b7150c3a12cb8d741be6
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-11-24 07:15:53 +0800

    feat: Replace navbar with tagline

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -1,27 +1,38 @@
+// function NavBarLayout() {
+
+//     const navItems = [
+//         { href: "#about", label: "About" },
+//         { href: "#skills", label: "Skills" },
+//         { href: "#achievements", label: "Achievements" },
+//         { href: "#qualifications", label: "Qualifications" },
+//         { href: "#projects", label: "Projects" },
+//         { href: "#contact", label: "Contact" },
+//     ]
+
+//     return (
+//         <header className="flex justify-between items-center mb-8 max-w-2xl mx-auto">
+//             <div className="flex space-x-4">
+//                 {/* <button className="text-2xl">☼</button>
+//                 <button className="text-2xl">☾</button>
+//                 <button className="text-2xl">⧉</button> */}
+//             </div>
+//             <nav className="overflow-auto">
+//                 {
+//                     navItems.map((item, index) => (
+//                         <a key={index} href={item.href} className="mr-4 underline">{item.label}</a>
+//                     ))
+//                 }
+//             </nav>
+//         </header>
+//     )
+// }
+
 function NavBarLayout() {
 
-    const navItems = [
-        { href: "#about", label: "About" },
-        { href: "#skills", label: "Skills" },
-        { href: "#achievements", label: "Achievements" },
-        { href: "#qualifications", label: "Qualifications" },
-        { href: "#projects", label: "Projects" },
-        { href: "#contact", label: "Contact" },
-    ]
-
     return (
-        <header className="flex justify-between items-center mb-8 max-w-2xl mx-auto">
-            <div className="flex space-x-4">
-                {/* <button className="text-2xl">☼</button>
-                <button className="text-2xl">☾</button>
-                <button className="text-2xl">⧉</button> */}
-            </div>
+        <header className="flex justify-center items-center mb-8 max-w-2xl mx-auto">
             <nav className="overflow-auto">
-                {
-                    navItems.map((item, index) => (
-                        <a key={index} href={item.href} className="mr-4 underline">{item.label}</a>
-                    ))
-                }
+                <h1>RipinDensumite Portfolio</h1>
             </nav>
         </header>
     )
