3eb15615e977290e719cd5461ba29d5704ecb509
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-11-24 07:17:21 +0800

    feat: Redesign Skills UI

--- src/modules/skills/index.tsx
+++ src/modules/skills/index.tsx
@@ -1,21 +1,64 @@
 export default function SkillsModule() {
+
+    const skills = [
+        {
+            title: "Languages",
+            skills: [
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "PHP",
+                "Java",
+                "SQL",
+                "C"
+            ]
+        },
+        {
+            title: "Frameworks & Libraries",
+            skills: [
+                "React.js",
+                "Next.js",
+                "Node.js",
+                "Vite",
+                "Tailwind CSS",
+            ]
+        },
+        {
+            title: "Developer Tools",
+            skills: [
+                "Docker",
+                "Git",
+                "Caddy",
+                "Pocketbase",
+                "XAMPP",
+            ]
+        }
+    ]
+
     return (
         <section id="skills">
             <h2 className="text-2xl font-bold mb-4">SKILLS</h2>
-            <ul className="list-disc list-inside">
-                <li>JavaScript / TypeScript</li>
-                <li>React.js / Next.js</li>
-                <li>Node.js</li>
-                <li>HTML / CSS3 / Tailwind CSS</li>
-                <li>PHP</li>
-                <li>C</li>
-                <li>JAVA</li>
-                <li>Git / GitHub</li>
-                <li>Docker</li>
-                <li>Linux</li>
-                <li>Windows</li>
-                <li>SQL</li>
-            </ul>
+            <div className="flex flex-col gap-5">
+                {
+                    skills.map((skill, index) => (
+                        <div key={index} className="flex flex-col">
+                            <h1 className="mb-2 font-semibold text-lg">{skill.title}</h1>
+                            <div className="flex flex-wrap gap-2">
+                                {
+                                    skill.skills.map((skill, index) => (
+                                        <span
+                                            key={index}
+                                            className="px-3 py-1 h-fit w-fit bg-zinc-700/10 text-slate-700 rounded-md text-sm font-medium hover:bg-blue-500/20 transition-colors duration-200 cursor-default"
+                                        >
+                                            {skill}
+                                        </span>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </section>
     )
 }
