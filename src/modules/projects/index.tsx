0b6ba57f3fcad5729589db2b62655897b6ce3194
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-11-24 14:51:21 +0800

    Update index.tsx

--- src/modules/projects/index.tsx
+++ src/modules/projects/index.tsx
@@ -21,7 +21,7 @@ export default function ProjectsModule() {
             description: "Web-based marketing and management system to streamline marketing efforts for staff and enhancing their ability to attract visitors and access materials efficiently.",
             techStack: ["Next.js", "React.js", "Tailwind CSS", "Pocketbase", "Docker"],
             url: "https://mams.site/",
-            status: "live",
+            status: "",
             isWorkingOn: true
         },
         // {
@@ -36,7 +36,7 @@ export default function ProjectsModule() {
             description: "Simple shopping cart system made during web development course.",
             techStack: ["PHP", "MySQL"],
             url: "https://pharmagains.ripin.live/",
-            status: "live",
+            status: "",
             isWorkingOn: false
         },
         {
@@ -60,18 +60,22 @@ export default function ProjectsModule() {
                             className="bg-slate-50 border-2 rounded-md border-gray-800 p-5 flex flex-row justify-between transition-transform duration-300"
                         >
                             <section>
-                                <h3 className="font-bold text-slate-800 flex items-center">
-                                    {project.name}
-                                    {project.isWorkingOn && (
-                                        <span className="ml-2 flex items-center">
-                                            <span className="relative flex h-3 w-3">
-                                                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                                                <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                                <div className='flex items-center justify-between'>
+                                    <h3 className="font-bold text-slate-800 flex items-center">
+                                        {project.name}
+                                        {project.isWorkingOn && (
+                                            <span className="ml-2 hidden items-center sm:flex">
+                                                <span className="relative flex h-3 w-3">
+                                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+                                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                                                </span>
+                                                <span className="ml-2 text-sm text-green-700">Working on</span>
                                             </span>
-                                            <span className="ml-2 text-sm text-green-700">Working on</span>
-                                        </span>
-                                    )}
-                                </h3>
+                                        )}
+                                    </h3>
+                                    {project.status === "live" && <span className="bg-emerald-100 px-2.5 py-0.5 border-2 border-emerald-700 text-emerald-700 h-fit w-fit">live</span>}
+                                    {project.status === "offline" && <span className="bg-red-100 px-2.5 py-0.5 border-2 border-red-700 text-red-700 h-fit w-fit">offline</span>}
+                                </div>
                                 <p className='text-slate-600 text-sm'>{project.description}</p>
                                 <div className=' gap-2 items-center mt-2 mb-2'>
                                     <span className='w-28'>Tech stack</span>
@@ -89,8 +93,6 @@ export default function ProjectsModule() {
                                 </div>
                                 <a href={project.url} className="underline">View Project</a>
                             </section>
-                            {project.status === "live" && <span className="bg-emerald-100 px-2.5 py-0.5 border-2 border-emerald-700 text-emerald-700 h-fit">live</span>}
-                            {project.status === "offline" && <span className="bg-red-100 px-2.5 py-0.5 border-2 border-red-700 text-red-700 h-fit">offline</span>}
                         </div>
                     ))
                 }
