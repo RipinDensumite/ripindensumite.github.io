5274d2a26c7ce2e08623d5ace9ccfae08d6ce25a
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-12-12 14:25:37 +0800

    Replace App.tsx with home.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './pages/home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,49 @@
+import NavBarLayout from '../../layout/navbar'
+import AboutModule from '../../modules/about'
+import SkillsModule from '../../modules/skills'
+import AchievementsModule from '../../modules/achievements'
+import CertificatesModule from '../../modules/certificates'
+// import QualificationsModule from '../../modules/qualifications'
+import ProjectsModule from '../../modules/projects'
+import ContactModule from '../../modules/contact'
+import { TextFade } from '../../components/text animation/TextFade'
+
+function App() {
+    return (
+        <div className="min-h-screen bg-white text-gray-800 font-mono p-8 overflow-auto bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+            {/* <section className='max-w-2xl mx-auto px-5 py-3'> */}
+            <NavBarLayout />
+
+            <main className="max-w-2xl mx-auto space-y-16">
+                <TextFade direction="up">
+                    <AboutModule />
+                </TextFade>
+                <TextFade direction="up">
+                    <SkillsModule />
+                </TextFade>
+                <TextFade direction="up">
+                    <CertificatesModule />
+                </TextFade>
+                <TextFade direction="up">
+                    <AchievementsModule />
+                </TextFade>
+                {/* <TextFade direction="up">
+          <QualificationsModule />
+        </TextFade> */}
+                <TextFade direction="up">
+                    <ProjectsModule />
+                </TextFade>
+                <TextFade direction="up">
+                    <ContactModule />
+                </TextFade>
+            </main>
+
+            <footer className="mt-16 text-center text-sm text-gray-500">
+                © 2024 RipinDensumite :: Aspiring Software Engineer
+            </footer>
+            {/* </section> */}
+        </div>
+    )
+}
+
+export default App
