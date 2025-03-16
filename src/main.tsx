d3eb5236f1e5252497b3e44c7cd00e99c7b266bc
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-16 16:38:13 +0800

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './pages/home'
-import ErrorPage from './pages/404'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <BrowserRouter>
-      <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='*' element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
